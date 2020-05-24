8a30e8f5fb14d67427f70215331d2e793e61c402
Lukáš Cehlár <lukas.cehlar@digital.cz>
2020-05-24 21:52:31 +0200

    [FEA] - added button for jokes page

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
 		<h2 class="text-3xl text-blue-500 font-semibold mb-4 pb-4 border-b-2">
 			Welcome to the best dad jokes app
 		</h2>
+		<nuxt-link to="/jokes" class="bg-blue-600 block duration-200 hover:bg-blue-800 inline-block px-8 py-2 rounded-lg font-semibold text-lg text-white transition-colors mb-4">Go to Jokes</nuxt-link>
 	</div>
 </template>
 
