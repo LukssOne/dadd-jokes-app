950886bb5634b5a0d2645c6a9c887b1d99dcc7b3
Lukáš Cehlár <lukas.cehlar@digital.cz>
2020-05-24 18:06:00 +0200

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h2>
+			Welcome to the best dad jokes app
+		</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		head() {
+			return {
+				title: 'Welcome To Dad Jokes',
+				meta: [
+					{
+						hid: 'description',
+						name: 'description',
+						content: 'Best place for dad jokes',
+					}
+				],
+			};
+		}
+	}
+</script>
